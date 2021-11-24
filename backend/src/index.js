b166b5647e576d960bb8829acaeeeca8d61e218a
Bhavitha Innamuri <bhavithainnamuri@my.unt.edu>
2021-11-24 15:40:20 -0600

    last

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,7 +51,7 @@ app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors)
+// app.use(cors)
 app.use(cors({
     origin:["http://localhost:3000"]
 }));
