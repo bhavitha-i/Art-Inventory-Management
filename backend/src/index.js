087c05463d0e9f06ae2b967d1520daf23ecaac01
Bhavitha Innamuri <bhavithainnamuri@my.unt.edu>
2021-11-24 06:51:53 -0600

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,14 +51,14 @@ app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors)
 app.use(cors({
     origin:["http://localhost:3000"]
 }));
 
 console.log(__dirname)
-// const direc = path.join(__dirname, 'public')
 
-app.use(express.static(path.join(__dirname, '/public')))
+
   
 app.use(ArtistRouter);
 app.use(ArtStyleRouter);
@@ -89,6 +89,15 @@ app.use(Exhibition_TicketsRouter)
 app.use(Art_SuppliesRouter)
 app.use(Artist_PurchasesRouter)
 app.use(AddressRouter)
+
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
 app.listen(port,()=>{
     console.log('server is up on port '+ port)
