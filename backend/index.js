7161d253dc2907c36024d24c06965e5f77514817
bhavitha-i <91703455+bhavitha-i@users.noreply.github.com>
2021-11-24 18:43:48 -0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -91,7 +91,7 @@ app.use(Artist_PurchasesRouter)
 app.use(AddressRouter)
 
 
-app.use(express.static(path.join(__dirname, '/src/public')))
+app.use("/uploadImages",express.static(path.join(__dirname, '/src/public/uploadImages')))
 
 // app.use("/uploadImages",express.static(path.join(__dirname, '/public')))
 
