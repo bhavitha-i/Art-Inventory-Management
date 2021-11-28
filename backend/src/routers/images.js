96de506de358b7087375aa08c4b33f991d44f002
Bhavitha Innamuri <bhavithainnamuri@my.unt.edu>
2021-11-28 15:03:37 -0600

    images to s3 bucket

--- backend/src/routers/images.js
+++ backend/src/routers/images.js
@@ -1,22 +1,27 @@
-const express = require("express");
-const router = express.Router();
+const AWS = require('aws-sdk')
 const multer = require("multer")
-// const path = require("path")
+const multerS3 = require("multer-s3")
+const express = require("express");
 require('dotenv').config();
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    signatureVersion: 'v4'
+});
 
 
-const storage = multer.diskStorage({
-    
-    destination: function (req, file, cb) {
-        cb(null, './src/public/uploadImages/');
-        console.log("Are we even here ?")
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "--" + file.originalname);
-    }
-});  
+const S3 = new AWS.S3();
+
+
+const getUniqFileName = (originalname) => {
+    const name = uuidv4();
+    const ext = originalname.split('.').pop();
+    return `${name}.${ext}`;
+}
 
 const fileFilter = (req, file, cb) => {
     if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpg')){
@@ -25,11 +30,31 @@ const fileFilter = (req, file, cb) => {
         cb(null, false);
 
     }
+}
 
-};
- 
-let upload = multer({ storage: storage, fileFilter: fileFilter});
 
+
+
+
+let upload = multer({ 
+    fileFilter,
+    storage: multerS3({
+        s3: S3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: function (req, file, cb) {
+            const fileName = getUniqFileName(file.originalname);
+            const s3_inner_directory = 'uploadImages';
+            const finalPath = `${s3_inner_directory}/${fileName}`;
+    
+            file.newName = fileName;
+    
+            cb(null, finalPath );
+        }
+    }), 
+   
+});
 
 
 //@type   POST
@@ -41,8 +66,8 @@ router.post("/uploadImage", upload.single('Image'), (req, res) => {
     if (!req.file) {
         console.log("No file upload");
     } else {
-        console.log(req.file.filename)
-        var imgsrc = process.env.EXPRESS_HOST+'uploadImages/' + req.file.filename
+        console.log(req.file.fieldname,req.file.location,'---- files')
+        var imgsrc = req.file.location
         console.log(imgsrc, "--imgsrc")
         res.status(200).json({
           status: true,
@@ -54,7 +79,3 @@ router.post("/uploadImage", upload.single('Image'), (req, res) => {
 
 
 module.exports = router;
-
-
-
-  
